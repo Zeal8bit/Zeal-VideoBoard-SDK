0c09c9308c0a59e8fd1473d92f72152bb43e02a6
Zeal8bit <contact@zeal8bit.com>
2024-11-08 23:22:05 +0800

    build: add a base Makefile for SDCC to simplify new programs compilation
    The new `base_sdcc.mk` file can now be included inside projects using ZVB SDK
    in order to ease compilation. This file is now used by all the C examples.

---
+++ sdcc/base_sdcc.mk
@@ -0,0 +1,27 @@
+#
+# SPDX-FileCopyrightText: 2024 Zeal 8-bit Computer <contact@zeal8bit.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This file is means to be included by programs based on SDCC.
+# This will ease writing a Makefile for a new project that is meant to use ZVB SDK.
+# This file can included by adding this line to any Makefile:
+#	include $(ZVB_SDK_PATH)/sdcc/base_sdcc.mk
+
+ifndef ZOS_PATH
+    $(error "Failure: ZOS_PATH variable not found. It must point to Zeal 8-bit OS path.")
+endif
+ifndef ZVB_SDK_PATH
+    $(error "Failure: ZVB_SDK_PATH variable not found. It must point to Zeal Video Board SDK path.")
+endif
+
+# Specify Z80 as the target, compile without linking, and place all the code in TEXT section
+ZVB_CFLAGS ?= -I$(ZVB_SDK_PATH)/include/
+ZOS_CFLAGS += $(ZVB_CFLAGS)
+
+# Make sure the whole program is relocated at 0x4000 as request by Zeal 8-bit OS.
+ZVB_LDFLAGS ?= -k $(ZVB_SDK_PATH)/lib/ -l zvb_gfx
+ZOS_LDFLAGS += $(ZVB_LDFLAGS)
+
+include $(ZOS_PATH)/kernel_headers/sdcc/base_sdcc.mk
