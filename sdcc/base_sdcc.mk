f20e911689e457cce9b8c531d7bb2c7b3f6b901e
David Higgins <zoul0813@me.com>
2024-11-14 13:58:03 -0500

    * ADD: ENABLE_SOUND and ENABLE_CRC32 to allow users to include the zvb_sound and zvb_crc32 libs

--- sdcc/base_sdcc.mk
+++ sdcc/base_sdcc.mk
@@ -19,9 +19,26 @@ endif
 # Specify Z80 as the target, compile without linking, and place all the code in TEXT section
 ZVB_CFLAGS ?= -I$(ZVB_SDK_PATH)/include/
 ZOS_CFLAGS += $(ZVB_CFLAGS)
+ENABLE_GFX ?= 1
+ENABLE_SOUND ?= 0
+ENABLE_CRC32 ?= 0
 
 # Make sure the whole program is relocated at 0x4000 as request by Zeal 8-bit OS.
-ZVB_LDFLAGS ?= -k $(ZVB_SDK_PATH)/lib/ -l zvb_gfx
+ZVB_LDFLAGS ?= -k $(ZVB_SDK_PATH)/lib/
+
+ifeq ($(ENABLE_GFX)), 1)
+ ZVB_FLAGS += -l zvb_gfx
+ endif
+
+ifeq ($(ENABLE_SOUND), 1)
+ZVB_LDFLAGS += -l zvb_sound
+endif
+
+ifeq ($(ENABLE_CRC32), 1)
+ZVB_LDFLAGS += -l zvb_crc32
+endif
+
 ZOS_LDFLAGS += $(ZVB_LDFLAGS)
+
 
 include $(ZOS_PATH)/kernel_headers/sdcc/base_sdcc.mk
