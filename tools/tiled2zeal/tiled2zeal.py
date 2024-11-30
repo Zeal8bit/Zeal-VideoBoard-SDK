5afab53bd172a7bdf563cb0a0bb9cf3270c9fb89
David Higgins <zoul0813@me.com>
2024-11-30 11:28:19 -0500

    * UPDATE: tiled2zeal - add support for Tiled layers, producing `filename-{Layer}.ztm` files if more than one layer exists

--- tools/tiled2zeal/tiled2zeal.py
+++ tools/tiled2zeal/tiled2zeal.py
@@ -9,35 +9,45 @@ parser.add_argument("-i", "--input", help="Input GIF Filename", required=True)
 parser.add_argument("-m", "--tilemap", help="Zeal Tilemap (ZTM)")
 
 def convert(args):
+  maps = []
   tree = ET.parse(args.input)
   root = tree.getroot()
-  # layer = root.find("layer")
-  data = root.find("layer/data")
 
-  if not data.attrib["encoding"] == 'csv':
-    print("Invalid Data Encoding, expected CSV and got ", data.attrib["encoding"])
-    return None
+  layers = root.findall("layer")
+  for layer in layers:
+    data = layer.find("data")
 
-  # tiled indexes are 1 based, so we subtract 1 to make it 0 based
-  tiles = [255 if int(i) < 1 else int(i) - 1 for i in data.text.replace("\n", "").split(",")]
-  return bytes(tiles)
+    if not data.attrib["encoding"] == 'csv':
+      print("Invalid Data Encoding, expected CSV and got ", data.attrib["encoding"])
+      return None
+
+    # tiled indexes are 1 based, so we subtract 1 to make it 0 based
+    tiles = [255 if int(i) < 1 else int(i) - 1 for i in data.text.replace("\n", "").split(",")]
+    maps.append(bytes(tiles))
+  return maps
 
 def main():
   args = parser.parse_args()
   print("args", args)
-  tilemap = convert(args)
-
-  if not tilemap:
+  maps = convert(args)
+  if not maps or len(maps) < 1:
     print("Failed to convert")
     return
 
-  tilemapFileName = args.tilemap
-  if tilemapFileName == None:
-    tilemapFileName = Path(args.input).with_suffix(".ztm")
+  for idx, tilemap in enumerate(maps):
+    if args.tilemap:
+      p = Path(args.tilemap)
+      index = ""
+      if len(maps) > 1:
+        index = f"-{str(idx)}"
+      tilemapFileName = p.parent / f"{p.stem}{index}{p.suffix}"
 
-  print("tilemap", tilemapFileName) #, tilemap)
-  with open(tilemapFileName, "wb") as file:
-    file.write(tilemap)
+    if tilemapFileName == None:
+      tilemapFileName = Path(args.input + "-" + str(idx)).with_suffix(".ztm")
+
+    print("tilemap", tilemapFileName) #, tilemap)
+    with open(tilemapFileName, "wb") as file:
+      file.write(tilemap)
 
 
 if __name__ == "__main__":
