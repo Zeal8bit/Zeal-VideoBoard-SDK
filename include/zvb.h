1153b2e984f7f2e0da0c698a4b14e817f4c53e3e
David Higgins <zoul0813@me.com>
2026-04-06 09:21:37 -0400

    add a new zvb.lib with zvb.h to enable linking against a single lib, and including a single header

---
+++ include/zvb.h
@@ -0,0 +1,15 @@
+/**
+ * SPDX-FileCopyrightText: 2026 Zeal 8-bit Computer <contact@zeal8bit.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "zvb_sdk.h"
+#include "zvb_hardware.h"
+#include "zvb_gfx.h"
+#include "zvb_sprite.h"
+#include "zvb_crc.h"
+#include "zvb_dma.h"
+#include "zvb_sound.h"
